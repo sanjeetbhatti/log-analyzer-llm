796d960da9533bb9096519ac994e9c6f47d28633
sanjeetbhatti <edu.sanjeet@gmail.com>
2026-06-29 19:56:52 -0600

    Read log file

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import argparse
+from pathlib import Path
 
 
 def _parse_args() -> argparse.Namespace:
@@ -6,13 +7,35 @@ def _parse_args() -> argparse.Namespace:
     sub = parser.add_subparsers(dest="command", required=True)
 
     analyze = sub.add_parser(name="analyze", help="Analyze a log file")
+    analyze.add_argument("log_file", type=Path, help="Path to log file")
+    analyze.add_argument("--output_dir", type=Path, default=Path("reports"), help="Report output dirctory")
 
     return parser.parse_args()
 
+def _analyze(log_file: Path, output_dir: Path):
+    if not log_file.exists() or not log_file.is_file():
+        print(f"Error: log file not found: {log_file}")
+        return
+
+    if not output_dir.exists():
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+    if not output_dir.is_dir():
+        print(f"Error: output dir could be found: {output_dir}")
+        return
+
+    print(f"analyzing: {log_file}")
+    log_text = log_file.read_text()
+    print(log_text)
+
+    print("Analysis complete")
+
 def main():
     args = _parse_args()
-    print(args)
 
+    if args.command == "analyze":
+        _analyze(args.log_file, args.output_dir)
+        
 
 if __name__ == "__main__":
     main()
